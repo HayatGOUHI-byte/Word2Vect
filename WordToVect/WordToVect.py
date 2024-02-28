9d78f18d2d75a453ba188c8af75ead9cd8990d53
developer123 <lo14ubna02@gmail.com>
2024-02-28 10:35:03 +0100

    these are changes to this file

--- WordToVect/WordToVect.py
+++ WordToVect/WordToVect.py
@@ -1,13 +1,19 @@
-from gensim.models import KeyedVectors
+from gensim.models import Word2Vec
 
-# Charger le modèle Word2Vec pré-entraîné
-model_path = "GoogleNews-vectors-negative300.bin.gz"
-word_vectors = KeyedVectors.load_word2vec_format(model_path, binary=True, limit=500000)  # Chargez seulement les premiers 500 000 mots pour cet exemple
+# Sample corpus (list of sentences)
+corpus = [
+    ["this", "is", "a", "sentence", "one"],
+    ["this", "is", "a", "sentence", "two"],
+    ["this", "is", "a", "sentence", "three"],
+    ["another", "sentence", "is", "here"],
+    ["yet", "another", "sentence", "is", "here"]
+]
 
-# Obtenir le vecteur correspondant au mot "car"
-word = "car"
-if word in word_vectors:
-    word_vector = word_vectors[word]
-    print(f"Vecteur pour le mot '{word}': {word_vector}")
-else:
-    print(f"Le mot '{word}' n'est pas présent dans le modèle Word2Vec.")
+# Train Word2Vec model
+model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=1, workers=4)
+
+# Find similar words
+similar_words = model.wv.most_similar("sentence")
+
+# Print similar words
+print("Similar words to 'sentence':", similar_words)
